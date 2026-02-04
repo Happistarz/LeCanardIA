54a991c644a6809527d2932f889bf80160f49add
Happistarz <mathieu.raveleau@gmail.com>
2026-02-04 22:08:37 +0100

    feat(ship-fsm): integrate ShipFSM management in main game loop

--- MyBot.cpp
+++ MyBot.cpp
@@ -2,6 +2,7 @@
 #include "hlt/constants.hpp"
 #include "hlt/log.hpp"
 #include "HaliteAI/Bot/bot_player.hpp"
+#include "HaliteAI/Bot/ship_fsm.hpp"
 
 #include <random>
 #include <ctime>
@@ -10,59 +11,85 @@ using namespace std;
 using namespace hlt;
 
 #ifdef _DEBUG
-# define LOG(X) log::log(X);
+#define LOG(X) log::log(X);
 #else
-# define LOG(X)
+#define LOG(X)
 #endif // DEBUG
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     unsigned int rng_seed;
-    if (argc > 1) {
+    if (argc > 1)
+    {
         rng_seed = static_cast<unsigned int>(stoul(argv[1]));
-    } else {
+    }
+    else
+    {
         rng_seed = static_cast<unsigned int>(time(nullptr));
     }
     mt19937 rng(rng_seed);
 
     Game game;
-    // At this point "game" variable is populated with initial map data.
-    // This is a good place to do computationally expensive start-up pre-processing.
-    // As soon as you call "ready" function below, the 2 second per turn timer will start.
     game.ready("LeCanardIA");
 
-    LOG("Toto")
+    std::vector<bot::ShipFSM *> ship_fsms;
 
-    for (;;) {
+    for (;;)
+    {
         game.update_frame();
         shared_ptr<Player> me = game.me;
-        unique_ptr<GameMap>& game_map = game.game_map;
+        unique_ptr<GameMap> &game_map = game.game_map;
 
         vector<Command> command_queue;
 
         bot::BotPlayer bot_player(game);
 
-        for (const auto& ship_iterator : me->ships) {
+        for (const auto &ship_iterator : me->ships)
+        {
             shared_ptr<Ship> ship = ship_iterator.second;
-            if (game_map->at(ship)->halite < constants::MAX_HALITE / 10 || ship->is_full()) {
-                Direction random_direction = ALL_CARDINALS[rng() % 4];
-                command_queue.push_back(ship->move(random_direction));
-            } else {
-                command_queue.push_back(ship->stay_still());
+
+            // Rechercher un ShipFSM existant
+            bot::ShipFSM *fsm = nullptr;
+            for (bot::ShipFSM *existing_fsm : ship_fsms)
+            {
+                if (existing_fsm->get_ship_id() == ship->id)
+                {
+                    fsm = existing_fsm;
+                    break;
+                }
             }
+
+            // Créer un nouveau ShipFSM si nécessaire
+            if (fsm == nullptr)
+            {
+                fsm = new bot::ShipFSM(ship->id);
+                ship_fsms.push_back(fsm);
+            }
+
+            Command command = fsm->update(ship, *game_map,
+                                          me->shipyard->position,
+                                          hlt::constants::MAX_TURNS - game.turn_number);
+            command_queue.push_back(command);
         }
 
-        if (
-            game.turn_number <= 200 &&
-            me->halite >= constants::SHIP_COST &&
-            !game_map->at(me->shipyard)->is_occupied())
+        // TODO: Améliorer la logique de spawn
+        if (game.turn_number <= 200 && me->halite >= hlt::constants::SHIP_COST)
         {
             command_queue.push_back(me->shipyard->spawn());
         }
 
-        if (!game.end_turn(command_queue)) {
+        if (!game.end_turn(command_queue))
+        {
             break;
         }
     }
+
+    // Cleanup
+    for (const bot::ShipFSM *fsm : ship_fsms)
+    {
+        delete fsm;
+    }
+    ship_fsms.clear();
 
     return 0;
 }
